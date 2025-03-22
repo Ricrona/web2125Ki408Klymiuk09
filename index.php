0dc0879523a7c32a8ff67edb405b405ab64fa597
Ricrona <alonaklymiuk@gmail.com>
2025-03-22 16:43:18 +0200

    add non AJAX forms

--- index.php
+++ index.php
@@ -10,16 +10,33 @@
   <p>Position: Student of KI</p>
   <p>Task repo: <a href="https://github.com/Ricrona/web2125Ki408Klymiuk09/tree/main" target="__blank">click here!</a></p>
   <p>Contact: alyona.klymiyk.ki.2021@lpnu.ua</p>
-  
-  <p><a href="get_page.php">Load GET Page</a></p>
 
-  <form action="post_page.php" method="POST">
-    <input type="hidden" name="info" value="Some POST data">
-    <button type="submit">Load POST Page</button>
+  <?php
+  if (isset($_GET['non_ajax_get'])) {
+      echo "<p>Non-AJAX GET value: " . htmlspecialchars($_GET['non_ajax_get']) . "</p>";
+  }
+  if (isset($_POST['non_ajax_post'])) {
+      echo "<p>Non-AJAX POST value: " . htmlspecialchars($_POST['non_ajax_post']) . "</p>";
+  }
+  ?>
+
+  <h2>Non-AJAX Forms</h2>
+  <h3>GET Form (Non-AJAX)</h3>
+  <form method="GET" action="">
+    <label for="non_ajax_get">Enter GET value:</label>
+    <input type="text" name="non_ajax_get" id="non_ajax_get">
+    <button type="submit">Submit GET</button>
+  </form>
+
+  <h3>POST Form (Non-AJAX)</h3>
+  <form method="POST" action="">
+    <label for="non_ajax_post">Enter POST value:</label>
+    <input type="text" name="non_ajax_post" id="non_ajax_post">
+    <button type="submit">Submit POST</button>
   </form>
 
   <hr>
-  
+
   <h2>AJAX Forms</h2>
 
   <h3>AJAX POST Form</h3>
